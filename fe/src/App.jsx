0c3a6958c2a3b8aabd70d92fe7530aa4a1cb9d4c
Dilip Jaiswal <dilipjaiswal8104@gmail.com>
2025-09-29 21:51:26 +0530

    react project

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -2,6 +2,7 @@ function App() {
   return (
     <div>
       <p className="text-9xl text-blue-900 bg-red-500">Hello Dust Bndr</p>
+      <p className="text-5xl text-red-600 pt-10">How are you</p>
     </div>
   )
 }
